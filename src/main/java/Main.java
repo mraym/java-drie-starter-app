ced4023771aaef5a121403d0a7030d2e034f83b1
Martin R <sizlaksystems@gmail.com>
2016-08-30 23:01:10 +0200

    Removing Heroku references and adding a /say-hello-drie endpoint

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,7 +11,7 @@ import spark.template.freemarker.FreeMarkerEngine;
 import spark.ModelAndView;
 import static spark.Spark.get;
 
-import com.heroku.sdk.jdbc.DatabaseUrl;
+import com.google.gson.Gson;
 
 public class Main {
 
@@ -22,38 +22,24 @@ public class Main {
 
     get("/hello", (req, res) -> "Hello World");
 
+    get("/say-hello-drie", (request, response) -> {
+      Map<String, String> messageMap = new HashMap<String,String>();
+      messageMap.put("message", "Hello Java Spark Microframework on Drie!");
+      
+      Gson gson = new Gson();
+      String json = gson.toJson(messageMap);
+
+      response.status(200);
+      response.type("application/json");
+      return json;
+    });
+
     get("/", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
             attributes.put("message", "Hello World!");
 
             return new ModelAndView(attributes, "index.ftl");
         }, new FreeMarkerEngine());
-
-    get("/db", (req, res) -> {
-      Connection connection = null;
-      Map<String, Object> attributes = new HashMap<>();
-      try {
-        connection = DatabaseUrl.extract().getConnection();
-
-        Statement stmt = connection.createStatement();
-        stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-        ArrayList<String> output = new ArrayList<String>();
-        while (rs.next()) {
-          output.add( "Read from DB: " + rs.getTimestamp("tick"));
-        }
-
-        attributes.put("results", output);
-        return new ModelAndView(attributes, "db.ftl");
-      } catch (Exception e) {
-        attributes.put("message", "There was an error: " + e);
-        return new ModelAndView(attributes, "error.ftl");
-      } finally {
-        if (connection != null) try{connection.close();} catch(SQLException e){}
-      }
-    }, new FreeMarkerEngine());
 
   }
 
